457b78e5e30a75474fce72ae099ba709513dd59c
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-05-07 16:46:12 +0530

    feat: add database schema for files and folders management

---
+++ lib/db/schema.ts
@@ -0,0 +1,29 @@
+import { relations } from "drizzle-orm";
+import { pgTable, uuid, text, integer, boolean, timestamp } from "drizzle-orm/pg-core";
+
+export const files = pgTable("files",{
+    id: uuid("id").defaultRandom().primaryKey(),
+    // files and folders info
+    name: text("name").notNull(),
+    path: text("path").notNull(), // This path store nested system 'docs/myfile/hello'
+    size: integer("size").notNull(), 
+    type: text("type").notNull(), // "File / Folder" 
+
+    // Storage information
+    fileurl: text("file_url").notNull(),
+    thumbnailUrl: text("thumbnail_URL"),
+
+    // Ownership Info 
+    userId: text("user_id").notNull(),
+    parentId: uuid("parent_id"), //if null for root items 
+
+    // file / folders flags 
+    isFolder: boolean("is_folder").notNull().default(false),
+    isBookmarked: boolean("is_bookmarked").default(false).notNull(),
+    isTrash: boolean("is_trash").default(false).notNull(),
+
+    //Time stamps
+    createAt: timestamp("created_at").defaultNow().notNull(),
+    updatedAt: timestamp("updated_at").defaultNow().notNull()
+
+})
