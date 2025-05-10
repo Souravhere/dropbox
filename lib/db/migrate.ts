6b9851ab151cff7666982687fc8e5b2b2eb26a44
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-05-10 18:47:08 +0530

    feat: implement migration script with error handling and add db:migrate command to package.json

--- lib/db/migrate.ts
+++ lib/db/migrate.ts
@@ -1,6 +1,25 @@
 import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
-import { migrate } from "drizzle-orm/neon-serverless/migrator";
+import { migrate } from "drizzle-orm/neon-http/migrator";
 import * as dotenv from "dotenv";
 
-dotenv.config({path:".env"});
+dotenv.config({path: ".env"});
+if(!process.env.DATABASE_URL){
+    throw new Error("Data base url is not set in .env")
+}
+
+async function runMigration() {
+    try {
+        const sql = neon(process.env.DATABASE_URL!)
+        const db = drizzle(sql)
+
+        await migrate(db, {migrationsFolder: "./drizzle"});
+        console.log("All Migtations successfully done");
+        
+    } catch (error) {
+        console.log("All Migtations failed");
+        process.exit(1)
+    }
+}
+
+runMigration()
