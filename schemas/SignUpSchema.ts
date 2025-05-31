107d17a7b68be1c8a50f5a16d9bbf44281a782c3
Sourav Chhimpa <souravchhimpa1@gmail.com>
2025-05-31 18:28:23 +0530

    feat: extend signUpSchema to include password and password confirmation validation

--- schemas/SignUpSchema.ts
+++ schemas/SignUpSchema.ts
@@ -5,5 +5,12 @@ export const signUpSchema = z
         email: z
             .string()
             .min(1,{message:"Email is required"})
-            .email({message:"Please Enter a vaild email"})
+            .email({message:"Please Enter a vaild email"}),
+        password: z
+                .string()
+                .min(1,{message:"Password is required"})
+                .min(8,{message:"Password min lenght is 8 characters"}),
+        passwordConformation: z
+                .string()
+                .min(1,{message:"Password is not matched"})
     })
